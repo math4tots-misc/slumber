42291602b7d5e3277b6971e2662737f7a3652938
Kyumin Kim <math4tots@gmail.com>
2016-06-17 02:41:14 -0700

    javascript implementation WIP

---
+++ slumber.js
@@ -0,0 +1,568 @@
+/* jshint esversion: 6 */
+/*
+Language Notes:
+
+* TENTATIVE: Naming convention is based on 'permanence' of a value.
+
+  - Final references to immutable values are ALL_CAPS.
+  - Final references to mutable values CapitalizedCamelCase.
+  - All other values are camelCase.
+
+Following this rule should 'nil', 'true', 'false' be ALLCAPS?
+
+Maybe
+  - constants that are data should be ALL_CAPS,
+  - declared classes should be CapitalizedCamelCase,
+  - and all other names should be camelCase.
+
+This means that e.g. if a function argument is a class, the name of the
+argument is camelCase and not CapitalizedCamelCase.
+
+Implementation Notes:
+
+* I try to avoid special attribute names (like 'type').
+So for instance when I would normally have used 'type', I use 'typ'.
+
+*/
+
+////// source
+
+class Source {
+  constructor(uri, dat) {
+    this.uri = uri;  // string (where dat came from)
+    this.dat = dat;  // string (contents of program)
+  }
+}
+
+class Token {
+  constructor(src, pos, typ, val) {
+    this.src = src;  // Source
+    this.pos = pos;  // int (location in src.dat)
+    this.typ = typ;  // string (token type)
+    this.val = val;  // any (token value, depends on typ [sic])
+  }
+
+  getLineNumber() {
+    let lineNumber = 1;
+    for (let i = 0; i < this.pos; i++) {
+      if (this.src.dat[i] === '\n') {
+        lineNumber++;
+      }
+    }
+    return lineNumber;
+  }
+
+  getColumnNumber() {
+    let columnNumber = 0;
+    for (let i = this.pos; i >= 0 && this.src.dat[i] !== '\n'; i--) {
+      columnNumber++;
+    }
+    return columnNumber;
+  }
+
+  getLine() {
+    let str = this.src.dat;
+    let lineStart = this.pos;
+    while (lineStart > 0 && str[lineStart-1] !== '\n') {
+      lineStart--;
+    }
+    let lineEnd = this.pos;
+    while (lineEnd < str.length && str[lineEnd] !== '\n') {
+      lineEnd++;
+    }
+    return str.slice(lineStart, lineEnd);
+  }
+
+  getLocationMessage() {
+    return (
+        '\nFile "' + this.src.uri + '", line ' + this.getLineNumber() +
+        '\n' + this.getLine() +
+        '\n' + ' '.repeat(this.getColumnNumber()-1) + '*');
+  }
+
+  toString() {
+    return (
+        'Token(source, ' + this.pos + ', ' + this.typ + ', ' +
+        this.val + ')');
+  }
+
+  inspect() {
+    return this.toString();
+  }
+}
+
+class SlumberError extends Error {
+  constructor(message, token) {
+    super(message);
+    this.slumberTrace = [];
+    if (token) {
+      this.addTokenToSlumberTrace(token);
+    }
+  }
+
+  addTokenToSlumberTrace(token) {
+    this.slumberTrace.push(token);
+  }
+
+  getSlumberTrace() {
+    let trace = '';
+    for (let token of this.slumberTrace) {
+      trace += token.getLocationMessage();
+    }
+    return trace;
+  }
+
+  toString() {
+    return (
+        super.toString() +
+        '\n--- slumber trace ---' + this.getSlumberTrace() +
+        '\n\n--- javascript trace --- \n' + this.stack);
+  }
+}
+
+////// lexer
+
+let KEYWORDS = [
+  // keywords from Python
+  'False', 'class', 'finally', 'is', 'return',
+  'None', 'continue', 'for', 'lambda', 'try',
+  'True', 'def', 'from', 'nonlocal', 'while',
+  'and', 'del', 'global', 'not', 'with',
+  'as', 'elif', 'if', 'or', 'yield',
+  'assert', 'else', 'import', 'pass',
+  'break', 'except', 'in', 'raise',
+  // my keywords
+  'async', 'await', 'self',
+];
+
+let SYMBOLS = [
+  // operators
+  '+', '-', '*', '**', '/', '//', '%', '@',
+  '<<', '>>', '&', '|', '^', '~',
+  '<', '>', '<=', '>=', '==', '!=',
+  // delimiters
+  '(', ')', '[', ']', '{', '}',
+  ',', ':', '.', ';', '@', '=', '->',
+  '+=', '-=', '*=', '/=', '//=', '%=', '@=',
+  '&=', '|=', '^=', '>>=', '<<=', '**=',
+  // -- ellipsis -- special token.
+  '...',
+];
+
+let ESCAPE_TABLE = {
+  '\\': '\\',
+  "'": "'",
+  '"': '"',
+  'n': '\n',
+  't': '\t',
+  'r': '\r',
+  'f': '\f',
+};
+
+function isSpace(c) {
+  return ' \t\n\r\f'.indexOf(c) !== -1;
+}
+
+function isDigit(c) {
+  return '0123456789'.indexOf(c) !== -1;
+}
+
+function isAlpha(c) {
+  if (c === undefined) {
+    return false;
+  }
+  return 'abcdefghijklmnopqrstuvwxyz'.indexOf(c.toLowerCase()) !== -1;
+}
+
+function isAlnum(c) {
+  return isDigit(c) || isAlpha(c);
+}
+
+function isWordChar(c) {
+  return c === '_' || isAlnum(c);
+}
+
+function lex(source) {
+  if (!(source instanceof Source)) {
+    throw new Error('lex requires a Source instance as argument');
+  }
+
+  let tokens = [];
+  let s = source.dat;
+  let i = 0;
+  let indentStack = [0];
+  let parenDepth = 0;
+
+  function skipEmptyLine() {
+    if (i >= s.length) {
+      return false;
+    }
+
+    let j = i;
+    while (j < s.length && s[j] !== '\n') {
+      if (s[j] === '#') {  // skip comments
+        while (j < s.length && s[j] !== '\n') {
+          j++;
+        }
+      } else if (isSpace(s[j])) {  // skip spaces
+        j++;
+      } else {  // we've hit a non-whitespace, non-comment character
+        return false;
+      }
+    }
+    // skipping an empty line includes skipping the terminating newline
+    if (s[j] === '\n') {
+      j++;
+    }
+    i = j;
+    return true;
+  }
+
+  function skipEmptyLines() {
+    while (skipEmptyLine());
+  }
+
+  function skipSpaces() {
+    while (true) {
+      if (s[i] === '\\' && s[i+1] === '\n') {
+        // escaped newline
+        i += 2;
+      } else if (s[i] === '#') {
+        // comments
+        while (i < s.length && s[i] !== '\n') {
+          i++;
+        }
+      } else if (isSpace(s[i]) && (s[i] !== '\n' || parenDepth > 0)) {
+        // spaces, and newlines only if we are nested inside parenthesis
+        i++;
+      } else {
+        break;
+      }
+    }
+  }
+
+  function processIndentation() {
+    let j = i;
+    while (s[j] === ' ') {
+      j++;
+    }
+    if (s[j] === '\t') {
+      throw new SlumberError(
+          'Indentation with tabs are not allowed',
+          new Token(source, j, 'ERR'));
+    }
+    let depth = j - i;
+    let lastIndent = indentStack[indentStack.length - 1];
+    if (lastIndent < depth) {
+      indentStack.push(depth);
+      tokens.push(new Token(source, j, 'INDENT'));
+    } else if (lastIndent > depth) {
+      while (indentStack[indentStack.length-1] > depth) {
+        indentStack.pop();
+        tokens.push(new Token(source, j, 'DEDENT'));
+      }
+      if (indentStack[indentStack.length-1] !== depth) {
+        throw new SlumberError(
+            'Invalid indentation depth',
+            new Token(source, j, 'ERR'));
+      }
+    }
+  }
+
+  function startsWithOneOf(prefixes, i) {
+    for (let prefix of prefixes) {
+      if (s.startsWith(prefix, i)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // tokenizing logic
+  while (true) {  // for each line
+    skipEmptyLines();
+    processIndentation();
+    if (i >= s.length) {
+      break;
+    }
+    while (true) {  // for each token on this line
+      skipSpaces();
+
+      // NEWLINE
+      if (i >= s.length || s[i] === '\n') {
+        tokens.push(new Token(source, i, 'NEWLINE'));
+        break;
+      }
+
+      // STRING
+      if (startsWithOneOf(['"', "'", 'r"""', "r'''"], i)) {
+        let j = i;
+        let raw = false;
+        if (s[i] === 'r') {
+          i++;
+          raw = true;
+        }
+        let value = '';
+        let quote;
+        if (startsWithOneOf(['"""', "'''"], i)) {
+          quote = s.substring(i, i+3);
+        } else {
+          quote = s[i];
+        }
+        i += quote.length;
+        while (!s.startsWith(quote, i)) {
+          if (i >= s.length) {
+            throw new SlumberError(
+                'Unterminated string literal',
+                new Token(source, j, 'ERR'));
+          } else if (!raw && s[i] === '\\') {
+            i++;
+            let escape = ESCAPE_TABLE[s[i]];
+            if (!escape) {
+              throw new SlumberError(
+                  'Invalid string escape: ' + s[i],
+                  new Token(source, i, 'ERR'));
+            }
+            value += escape;
+            i++;
+          } else {
+            value += s[i];
+            i++;
+          }
+        }
+        i += quote.length;
+        tokens.push(new Token(source, j, 'STRING', value));
+        continue;
+      }
+
+      // NUMBER
+      if (isDigit(s[i]) || s[i] === '.' && isDigit(s[i+1])) {
+        let j = i;
+        while (isDigit(s[i])) {
+          i++;
+          if (s[i] === '.') {
+            i++;
+            while (isDigit(s[i])) {
+              i++;
+            }
+          }
+        }
+        let value = parseFloat(s.substring(j, i));
+        tokens.push(new Token(source, j, 'NUMBER', value));
+        continue;
+      }
+
+      // NAME/KEYWORDS
+      if (isWordChar(s[i])) {
+        let j = i;
+        while (isWordChar(s[i])) {
+          i++;
+        }
+        let word = s.substring(j, i);
+        if (KEYWORDS.indexOf(word) !== -1) {
+          tokens.push(new Token(source, j, word));
+        } else {
+          tokens.push(new Token(source, j, 'NAME', word));
+        }
+        continue;
+      }
+
+      // SYMBOLS
+      let symbol = '';
+      for (let sym of SYMBOLS) {
+        if (s.startsWith(sym, i) && sym.length > symbol) {
+          symbol = sym;
+        }
+      }
+      if (symbol !== '') {
+        if ('([{'.indexOf(symbol) !== -1) {
+          parenDepth++;
+        } else if ('}])'.indexOf(symbol) !== -1) {
+          parenDepth--;
+        }
+        tokens.push(new Token(source, i, symbol));
+        i += symbol.length;
+        continue;
+      }
+
+      // ERR
+      let j = i;
+      while (i < s.length && !isSpace(s[i])) {
+        i++;
+      }
+      throw new SlumberError(
+          'Unrecognized token', new Token(source, j, 'ERR'));
+    }
+  }
+  tokens.push(new Token(source, i, 'EOF'));
+  return tokens;
+}
+
+////// ast
+
+class Ast {
+  constructor(token) {
+    this.token = token;
+  }
+}
+
+class FileInput extends Ast {
+  constructor(token, statements) {
+    super(token);
+    this.block = new Block(token, statements);
+  }
+
+  accept(visitor) {
+    return visitor.visitFileInput(this);
+  }
+}
+
+class Block extends Ast {
+  constructor(token, stmts) {
+    super(token);
+    this.stmts = stmts;  // [Statement]
+  }
+
+  accept(visitor) {
+    return visitor.visitBlock(this);
+  }
+}
+
+class ArgumentList extends Ast {
+  constructor(token, args, optargs, vararg) {
+    super(token);
+    this.args = args;  // [string]
+    this.optargs = optargs;  // [string]
+    this.vararg = vararg;  // string|undefined
+  }
+
+  accept(visitor) {
+    return visitor.visitArgumentList(this);
+  }
+}
+
+class ExpressionList extends Ast {
+  constructor(token, exprs, varexpr) {
+    super(token);
+    this.exprs = exprs;  // [Expression]
+    this.varexpr = varexpr;  // Expression|undefined
+  }
+
+  accept(visitor) {
+    return visitor.visitExpressionList(this);
+  }
+}
+
+class Expression extends Ast {}
+
+class NumberLiteral extends Expression {
+  constructor(token, val) {
+    super(token);
+    this.val = val;
+  }
+
+  accept(visitor) {
+    return visitor.visitNumerLiteral(this);
+  }
+}
+
+class Name extends Expression {
+  constructor(token, nam) {
+    super(token);
+    this.nam = nam;
+  }
+
+  accept(visitor) {
+    visitor.visitName(this);
+  }
+}
+
+class SimpleAssignment extends Expression {
+  constructor(token, nam, expression) {
+    super(token);
+    this.nam = nam;
+    this.expression = expression;
+  }
+
+  accept(visitor) {
+    visitor.visitSimpleAssignment(this);
+  }
+}
+
+
+
+////// tests
+
+function assert(condition, message) {
+  if (!condition) {
+    message = message ? message : 'assertion error';
+    throw new Error(message);
+  }
+}
+
+// simple source tests
+{
+  let src = new Source('<test>', 'abc');
+  let token = new Token(src, 1, 'typ', 'val');
+  let r = token.toString();
+  assert(r === 'Token(source, 1, typ, val)', r);
+  r = token.getLocationMessage();
+  assert(r === `
+File "<test>", line 1
+abc
+ *`, r);
+}
+
+// trivial lexer test
+{
+  let dat = `
+`;
+  let src = new Source('<test>', dat);
+  let tokens = lex(src);
+  assert(tokens.length === 1, tokens.length);
+  assert(tokens[0].typ === 'EOF', tokens[0].typ);
+}
+
+// simple lexer test
+{
+  let dat = `
+while True:
+  'hello\\n world' 5 4.4 x.y # hoi
+# fun`;
+  let src = new Source('<test>', dat);
+  let tokens = lex(src);
+  assert(tokens.length === 14, tokens.length);
+
+  assert(tokens[0].typ === 'while', tokens[0].typ);
+
+  assert(tokens[1].typ === 'True', tokens[1].typ);
+
+  assert(tokens[2].typ === ':', tokens[2].typ);
+
+  assert(tokens[3].typ === 'NEWLINE', tokens[3].typ);
+
+  assert(tokens[4].typ === 'INDENT', tokens[4].typ);
+
+  assert(tokens[5].typ === 'STRING', tokens[5].typ);
+  assert(tokens[5].val === 'hello\n world', tokens[5].val);
+
+  assert(tokens[6].typ === 'NUMBER', tokens[6].typ);
+  assert(tokens[6].val === 5, tokens[6].val);
+
+  assert(tokens[7].typ === 'NUMBER', tokens[7].typ);
+  assert(tokens[7].val === 4.4, tokens[7].val);
+
+  assert(tokens[8].typ === 'NAME', tokens[8].typ);
+  assert(tokens[8].val === 'x', tokens[8].val);
+
+  assert(tokens[9].typ === '.', tokens[9].typ);
+
+  assert(tokens[10].typ === 'NAME', tokens[10].typ);
+  assert(tokens[10].val === 'y', tokens[10].val);
+
+  assert(tokens[11].typ === 'NEWLINE', tokens[11].typ);
+
+  assert(tokens[12].typ === 'DEDENT', tokens[12].typ);
+
+  assert(tokens[13].typ === 'EOF', tokens[13].typ);
+}
