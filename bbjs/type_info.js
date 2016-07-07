64e70968bac8e8458eac284b5f8262252895462c
Kyumin Kim <math4tots@gmail.com>
2016-07-06 18:05:08 -0700

    bbjs/type_info_test pass!

--- bbjs/type_info.js
+++ bbjs/type_info.js
@@ -18,6 +18,34 @@ Usage:
 var TypeInfo = exports.TypeInfo = (function() {
   "use strict";
 
+  function isPrimitive(typename) {
+    return (
+        typename === 'void' ||
+        typename === 'bool' ||
+        typename === 'int' ||
+        typename === 'float');
+  }
+
+  function isBuiltin(typename) {
+    return (
+        typename === 'Object' ||
+        typename === 'String' ||
+        typename === 'List');
+  }
+
+  function argTypesAreEqual(argtypes1, argtypes2) {
+    var i, len;
+    if (argtypes1.length !== argtypes2.length) {
+      return false;
+    }
+    for (i = 0, len = argtypes1.length; i < len; i++) {
+      if (argtypes1[i] !== argtypes2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
   function TypeInfo() {
     this.classes = Object.create(null);
 
@@ -34,17 +62,78 @@ var TypeInfo = exports.TypeInfo = (function() {
   };
 
   TypeInfo.prototype._getFullTypename = function(typename) {
+    if (typeof typename !== 'string') {
+      throw new Error("typename must be a string: " + typename);
+    }
+    if (isPrimitive(typename)) {
+      return typename;
+    }
     if (this.aliases[typename]) {
       return this.aliases[typename];
+    }
+    if (isBuiltin(typename)) {
+      return 'bb.lang.' + typename;
     }
     return this._getPackage() + '.' + typename;
   };
 
   TypeInfo.prototype.processAllModules = function(modules) {
-    var i;
+    var i, key;
     for (i = 0; i < modules.length; i++) {
       this._visit(modules[i]);
     }
+    for (key in this.classes) {
+      this._processClassInheritance(key);
+    }
+  };
+
+  TypeInfo.prototype._processClassInheritance = function(className) {
+    var klass = this.classes[className], baseName, base, subBaseName;
+    var newSupers, attrName, methodName;
+    if (klass === undefined) {
+      throw new Error("Class " + className + " does not exist");
+    }
+    if (klass.processed) {
+      return;
+    }
+    klass.processed = true;
+    for (baseName in klass.supers) {
+      this._processClassInheritance(baseName);
+    }
+    newSupers = Object.create(null);
+    newSupers[className] = true;
+    for (baseName in klass.supers) {
+      base = this._getClassFromName(baseName);
+      for (subBaseName in base.supers) {
+        newSupers[subBaseName] = true;
+      }
+      for (attrName in base.attributeTypes) {
+        if (klass.attributeTypes[attrName]) {
+          throw new Error("Clashing " + className + "." + attrName +
+                          " attribute definition in inheritance tree");
+        }
+        klass.attributeTypes[attrName] = base.attributeTypes[attrName];
+      }
+      for (methodName in base.methodReturnType) {
+        if (klass.methodReturnType[methodName]) {
+          if ((klass.methodReturnType[methodName] !==
+               base.methodReturnType[methodName]) ||
+              !argTypesAreEqual(
+                  klass.methodArgTypes[methodName],
+                  base.methodArgTypes[methodName])) {
+            throw new Error("Clashing method definition for " +
+                            className + "." + methodName + " -- " +
+                            " in order to override a function type " +
+                            "signatures must be identical");
+          }
+        } else {
+          klass.methodReturnType[methodName] =
+              base.methodReturnType[methodName];
+          klass.methodArgTypes[methodName] = base.methodArgTypes[methodName];
+        }
+      }
+    }
+    klass.supers = newSupers;
   };
 
   TypeInfo.prototype._visit = function(node) {
@@ -57,7 +146,7 @@ var TypeInfo = exports.TypeInfo = (function() {
 
   TypeInfo.prototype._visitModule = function(node) {
     var i;
-    this.package = node.pkg;
+    this.package = node.pkg.pkg;
     this.aliases = Object.create(null);
     for (i = 0; i < node.imports.length; i++) {
       this._visit(node.imports[i]);
@@ -70,12 +159,157 @@ var TypeInfo = exports.TypeInfo = (function() {
   };
 
   TypeInfo.prototype._visitImport = function(node) {
-    // TODO
+    this.aliases[node.alias] = node.pkg + '.' + node.name;
   };
 
   TypeInfo.prototype._visitClass = function(node) {
-    // TODO
+    var i, that = this;
+    var klass = Object.create(null);
+    var className = this._getFullTypename(node.name);
+    klass.name = className;
+    klass.supers = Object.create(null);
+    if (node.base === null) {
+      if (className !== 'bb.lang.Object') {
+        klass.supers['bb.lang.Object'] = true;
+      }
+    } else {
+      klass.supers[this._getFullTypename(node.base.name)] = true;
+    }
+    for (i = 0; i < node.interfaces.length; i++) {
+      klass.supers[this._getFullTypename(node.interfaces[i].name)] = true;
+    }
+    klass.staticAttributeTypes = Object.create(null);
+    klass.attributeTypes = Object.create(null);
+    for (i = 0; i < node.attrs.length; i++) {
+      if (node.attrs[i].isStatic) {
+        if (klass.staticAttributeTypes[node.attrs[i].name]) {
+          throw new Error(className + "." + node.attrs[i].name + " has " +
+                          "duplicate definition");
+        }
+        klass.staticAttributeTypes[node.attrs[i].name] =
+            this._getFullTypename(node.attrs[i].cls.name);
+      } else {
+        if (klass.attributeTypes[node.attrs[i].name]) {
+          throw new Error(className + "." + node.attrs[i].name + " has " +
+                          "duplicate definition");
+        }
+        klass.attributeTypes[node.attrs[i].name] =
+            this._getFullTypename(node.attrs[i].cls.name);
+      }
+    }
+    klass.staticMethodReturnType = Object.create(null);
+    klass.methodReturnType = Object.create(null);
+    klass.staticMethodArgTypes = Object.create(null);
+    klass.methodArgTypes = Object.create(null);
+    function extractTypeFromTypeArgPair(typeArgPair) {
+      return that._getFullTypename(typeArgPair[0].name);
+    }
+    for (i = 0; i < node.methods.length; i++) {
+      if (node.methods[i].isStatic) {
+        if (klass.staticMethodReturnType[node.methods[i].name]) {
+          throw new Error(className + "." + node.methods[i].name + " has " +
+                          "duplicate definition");
+        }
+        klass.staticMethodReturnType[node.methods[i].name] =
+            this._getFullTypename(node.methods[i].returns.name);
+        klass.staticMethodArgTypes[node.methods[i].name] =
+            node.methods[i].args.map(extractTypeFromTypeArgPair);
+      } else {
+        if (klass.methodReturnType[node.methods[i].name]) {
+          throw new Error(className + "." + node.methods[i].name + " has " +
+                          "duplicate definition");
+        }
+        klass.methodReturnType[node.methods[i].name] =
+            this._getFullTypename(node.methods[i].returns.name);
+        klass.methodArgTypes[node.methods[i].name] =
+            node.methods[i].args.map(extractTypeFromTypeArgPair);
+      }
+    }
+    klass.processed = false;
+    this.classes[className] = klass;
   };
+
+  TypeInfo.prototype._getClassFromName = function(className) {
+    var klass = this.classes[className];
+    if (!klass) {
+      throw new Error("Class " + className + " doesn't exist");
+    }
+    return klass;
+  };
+
+  TypeInfo.prototype.classExists = function(className) {
+    return isPrimitive(className) || !!this.classes[className];
+  };
+
+  TypeInfo.prototype.isSubclass = function(subclassName, className) {
+    if (isPrimitive(subclassName)) {
+      return subclassName === className;
+    }
+    return !!this._getClassFromName(subclassName).supers[className];
+  };
+
+  TypeInfo.prototype.getAttributeType = function(className, attributeName) {
+    var klass = this._getClassFromName(className);
+    var type = klass.attributeTypes[attributeName];
+    if (!type) {
+      throw new Error("Attribute " + className + '.' + attributeName +
+                      " doesn't exist");
+    }
+    return type;
+  };
+
+  TypeInfo.prototype.getStaticAttributeType = function(
+      className, attributeName) {
+    var klass = this._getClassFromName(className);
+    var type = klass.staticAttributeTypes[attributeName];
+    if (!type) {
+      throw new Error("Static attribute " + className + '.' + attributeName +
+                      " doesn't exist");
+    }
+    return type;
+  };
+
+  TypeInfo.prototype.getMethodType = function(className, methodName) {
+    var klass = this._getClassFromName(className);
+    var type = klass.methodReturnType[methodName];
+    if (!type) {
+      throw new Error("Method " + className + '.' + methodName +
+                      " doesn't exist");
+    }
+    return type;
+  };
+
+  TypeInfo.prototype.getStaticMethodType = function(className, methodName) {
+    var klass = this._getClassFromName(className);
+    var type = klass.staticMethodReturnType[methodName];
+    if (!type) {
+      throw new Error("Static method " + className + '.' + methodName +
+                      " doesn't exist");
+    }
+    return type;
+  };
+
+  TypeInfo.prototype.getMethodArgTypes = function(className, methodName) {
+    var klass = this._getClassFromName(className);
+    var types = klass.methodArgTypes[methodName];
+    if (!types) {
+      throw new Error("Method " + className + '.' + methodName +
+                      " doesn't exist");
+    }
+    return types;
+  };
+
+  TypeInfo.prototype.getStaticMethodArgTypes = function(
+      className, methodName) {
+    var klass = this._getClassFromName(className);
+    var types = klass.staticMethodArgTypes[methodName];
+    if (!types) {
+      throw new Error("Static method " + className + '.' + methodName +
+                      " doesn't exist");
+    }
+    return types;
+  };
+
 
   return TypeInfo;
 })();
--- bbjs/type_info_test.js
+++ bbjs/type_info_test.js
@@ -53,94 +53,117 @@ describe("TypeInfo", function() {
 
   describe("#classExists", function() {
     it("should return true when class has been declared", function() {
-      expect(typeInfo.classExists('local.one.A')).to.be(true);
+      expect(typeInfo.classExists('local.one.A')).to.equal(true);
     });
     it("should return false for non-existent classes", function() {
-      expect(typeInfo.classExists('local.one.DoesNotExist')).to.be(false);
+      expect(typeInfo.classExists('local.one.DoesNotExist')).to.equal(
+          false);
     });
     it("should return true for primitive types", function() {
-      expect(typeInfo.classExists('int')).to.be(true);
+      expect(typeInfo.classExists('int')).to.equal(true);
     });
   });
 
   describe("#isSubclass", function() {
     it("should return true for explicitly extended class", function() {
-      expect(typeInfo.isSubclass('local.one.B', 'local.one.A')).to.be(true);
+      expect(typeInfo.isSubclass('local.one.B', 'local.one.A')).to.equal(
+          true);
     });
     it("should respect implicit Object inheritance", function() {
       expect(typeInfo.isSubclass(
-          'local.one.A', 'bb.lang.Object')).to.be(true);
+          'local.one.A', 'bb.lang.Object')).to.equal(true);
     });
     it("should return true for class implementing interface", function() {
-      expect(typeInfo.isSubclass('local.one.A', 'local.one.Ia')).to.be(true);
+      expect(typeInfo.isSubclass('local.one.A', 'local.one.Ia')).to.equal(
+          true);
     });
     it("should return true for indirect inheritance", function() {
-      expect(typeInfo.isSubclass('local.one.C', 'local.one.A')).to.be(true);
+      expect(typeInfo.isSubclass('local.one.C', 'local.one.A')).to.equal(
+          true);
     });
     it("should return true for class indirectly implementing interface",
        function() {
-      expect(typeInfo.isSubclass('local.one.B', 'local.one.Ia')).to.be(true);
+      expect(typeInfo.isSubclass('local.one.B', 'local.one.Ia')).to.equal(
+          true);
     });
     it("should return false for class that doesn't inherit", function() {
-      expect(typeInfo.isSubclass('local.one.D', 'local.one.A')).to.be(false);
+      expect(typeInfo.isSubclass('local.one.D', 'local.one.A')).to.equal(
+          false);
     });
     it("primitives do not inherit from Object", function() {
-      expect(typeInfo.isSubclass('int', 'bb.lang.Object')).to.be(false);
+      expect(typeInfo.isSubclass('int', 'bb.lang.Object')).to.equal(false);
     });
     it("primitives are still subclasses of themselves", function() {
-      expect(typeInfo.isSubclass('int', 'int')).to.be(true);
+      expect(typeInfo.isSubclass('int', 'int')).to.equal(true);
     });
     it("primitives are not subclasses of each other", function() {
-      expect(typeInfo.isSubclass('int', 'false')).to.be(false);
+      expect(typeInfo.isSubclass('int', 'false')).to.equal(false);
     });
     it("should recognize inheritance across modules", function() {
-      expect(typeInfno.isSubclass('local.two.D', 'local.one.A')).to.be(true);
+      expect(typeInfo.isSubclass('local.two.D', 'local.one.A')).to.equal(
+          true);
     });
   });
 
   describe("#getAttributeType", function() {
     it("should recognize direct attribute", function(){
-      expect(typeInfo.getAttributeType('local.one.A', 'x')).to.be('int');
+      expect(typeInfo.getAttributeType('local.one.A', 'x')).to.equal('int');
     });
     it("should recognize inherited attribute", function() {
-      expect(typeInfo.getAttributeType('local.one.B', 'x')).to.be('int');
+      expect(typeInfo.getAttributeType('local.one.B', 'x')).to.equal('int');
     });
     it("should throw when it doesn't recognize attribute", function() {
-      expect(typeInfo.getAttributeType('local.one.A', 'y')).to.throw(Error);
+      expect(function() {
+        typeInfo.getAttributeType('local.one.A', 'y');
+      }).to.throw(Error);
+    });
+    it("should throw if it is a static attribute", function() {
+      expect(function() {
+        typeInfo.getAttributeType('local.one.A', 'a');
+      }).to.throw(Error);
     });
   });
 
   describe("#getStaticAttributeType", function() {
     it("should recognize direct attribute", function(){
-      expect(typeInfo.getStaticAttributeType('local.one.A', 'a')).to.be(
+      expect(typeInfo.getStaticAttributeType('local.one.A', 'a')).to.equal(
           'bb.lang.String');
     });
-    it("should not inherited attribute", function() {
-      expect(typeInfo.getStaticAttributeType('local.one.B', 'x')).to.throw(
-          Error);
+    it("should not inherit static attribute", function() {
+      expect(function() {
+        typeInfo.getStaticAttributeType('local.one.B', 'a');
+      }).to.throw(Error);
+    });
+    it("should throw on member attribute", function() {
+      expect(function() {
+        typeInfo.getStaticAttributeType('local.one.A', 'x');
+      }).to.throw(Error);
     });
   });
 
   describe('#getMethodType', function() {
     it("should recognize direct method", function() {
-      expect(typeInfo.getMethodType('local.one.A', 'f')).to.be('int');
+      expect(typeInfo.getMethodType('local.one.A', 'f')).to.equal('int');
     });
     it("should recognize inherited method", function() {
-      expect(typeInfo.getMethodType('local.one.B', 'f')).to.be('int');
+      expect(typeInfo.getMethodType('local.one.B', 'f')).to.equal('int');
     });
     it("should throw when it doesn't recognize method", function() {
-      expect(typeInfo.getMethodType('local.one.A', 'g')).to.throw(Error);
+      expect(function() {
+        typeInfo.getMethodType('local.one.A', 'g');
+      }).to.throw(Error);
     });
   });
 
   describe("#getStaticMethodType", function() {
     it("should recognize direct method", function(){
-      expect(typeInfo.getStaticMethodType('local.one.A', 'foo')).to.be(
+      expect(typeInfo.getStaticMethodType('local.one.A', 'foo')).to.equal(
           'bb.lang.String');
     });
     it("should not inherit method", function() {
-      expect(typeInfo.getStaticMethodType('local.one.B', 'foo')).to.throw(
-          Error);
+      expect(function() {
+        typeInfo.getStaticMethodType('local.one.B', 'foo');
+      }).to.throw(Error);
     });
   });
 
@@ -151,7 +174,9 @@ describe("TypeInfo", function() {
       );
     });
     it("should throw when it doesn't recognize method", function() {
-      expect(typeInfo.getMethodArgTypes('local.one.A', 'g')).to.throw(Error);
+      expect(function() {
+        typeInfo.getMethodArgTypes('local.one.A', 'g');
+      }).to.throw(Error);
     });
   });
 
@@ -161,9 +186,62 @@ describe("TypeInfo", function() {
         .to.deep.equal(['int', 'float']);
     });
     it("should throw when it doesn't recognize method", function() {
-      expect(typeInfo.getStaticMethodArgTypes('local.one.A', 'g')).to.throw(
-          Error);
+      expect(function() {
+        typeInfo.getStaticMethodArgTypes('local.one.A', 'g');
+      }).to.throw(Error);
     });
   });
 
+  describe("#processAllModules", function() {
+    it("should throw on duplicate attribute definition", function() {
+      var typeInfo = new TypeInfo();
+      var module3 = `
+      package local.three;
+      class A {
+        int a;
+        float a;
+      }
+      `;
+      expect(function() {
+        typeInfo.processAllModules([
+          parser.parse(module0),
+          parser.parse(module3),
+        ]);
+      }).to.throw(Error);
+    });
+    it("should throw on duplicate method definition", function() {
+      var typeInfo = new TypeInfo();
+      var module3 = `
+      package local.three;
+      class A {
+        int a;
+        float a;
+      }
+      `;
+      expect(function() {
+        typeInfo.processAllModules([
+          parser.parse(module0),
+          parser.parse(module3),
+        ]);
+      }).to.throw(Error);
+    });
+    it("should throw on inheritance attribute clash", function() {
+      var typeInfo = new TypeInfo();
+      var module3 = `
+      package local.three;
+      class A {
+        int a;
+      }
+      class B extends A {
+        int a;
+      }
+      `;
+      expect(function() {
+        typeInfo.processAllModules([
+          parser.parse(module0),
+          parser.parse(module3),
+        ]);
+      }).to.throw(Error);
+    });
+  });
 });
